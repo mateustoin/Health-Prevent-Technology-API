24af0809a5f284db9d31ceddc19a29c0c9a9a191
Mateus Antonio <mateustoin@gmail.com>
2020-09-19 17:59:39 -0300

    feat(api): Adição de endpoints
    Ednpoints de busca de clientes através de parâmetros como idade mínima,
    idade máxima, intervalo e tipo de doenças.

--- main.py
+++ main.py
@@ -29,13 +29,40 @@ def get_db():
     finally:
         db.close()
 
-
 @app.get("/")
 def main():
     return RedirectResponse(url="/docs/")
 
-
 @app.get("/clientes/", response_model=List[schemas.Cliente])
 def show_records(db: Session = Depends(get_db)):
     records = db.query(models.Cliente).all()
+    return records
+
+@app.get("/clientes/doenca/{tipo_doenca}", response_model=List[schemas.Cliente])
+def retorna_pela_doenca(tipo_doenca: str, db: Session = Depends(get_db)):
+    records = db.query(models.Cliente).filter_by(doenca=tipo_doenca).all()
+    return records
+
+@app.get("/clientes/idade/{min}/{max}", response_model=List[schemas.Cliente])
+def retorna_intervalo_idade(min: int, max: int, db: Session = Depends(get_db)):
+    records = db.query(models.Cliente).filter(models.Cliente.idade >= min)
+    records = records.filter(models.Cliente.idade <= max).all()
+    
+    return records
+
+@app.get("/clientes/idade/menor/{idade}", response_model=List[schemas.Cliente])
+def retorna_menorque_idade(idade: int, db: Session = Depends(get_db)):
+    records = db.query(models.Cliente).filter(models.Cliente.idade <= idade).all()
+    
+    return records
+
+@app.get("/clientes/idade/maior/{idade}", response_model=List[schemas.Cliente])
+def retorna_maiorque_idade(idade: int, db: Session = Depends(get_db)):
+    records = db.query(models.Cliente).filter(models.Cliente.idade >= idade).all()
+    
+    return records
+
+@app.get("/notification/{tipo_doenca}", response_model=List[schemas.Cliente])
+def spread_notification(tipo_doenca: str, db: Session = Depends(get_db)):
+    records = db.query(models.Cliente).filter_by(doenca=tipo_doenca).all()
     return records
